63211709c2707b6d5e26190e3dd1cda608b48153
Michał Szyma <michal@szyma.dev>
2020-06-15 22:58:09 +0200

    Add Bootstrap-Vue theme

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
