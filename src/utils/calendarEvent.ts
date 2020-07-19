921d166d2a9871772b10290e014b37c2b6265ec9
Michał Szyma <michal@szyma.dev>
2020-07-20 00:32:38 +0200

    Improve showing a title of event in the calendar

--- src/utils/calendarEvent.ts
+++ src/utils/calendarEvent.ts
@@ -2,11 +2,13 @@ const Moment = require("moment");
 
 export class CalendarEvent
 {
-    _name : string;
-	_from : number;
-    _to : number
+    name: string;
+	  from: number;
+    to: number
     _start: string;
     _end: string;
+
+    title: string;
 	/**
 	 * 
 	 * @param {string} name 
@@ -15,51 +17,23 @@ export class CalendarEvent
 	 */
 	constructor(name : string, from : number, to : number)
 	{
-		this._name = name;
-		this._from = from;
-        this._to = to;
-        this._start = Moment.unix(from/1000).format('YYYY-MM-DD HH:mm:ss');
-        this._end = Moment.unix(to/1000).format('YYYY-MM-DD HH:mm:ss');;
-	}
-
-	/**
-	 * @type {string}
-	 */
-	get name() {
-
-		return this._name;
-	}
-
-    /**
-	 * @type {string}
-	 */
-	get title() {
-		return this._name;
-    }
-    
-	/**
-	 * @type {string}
-	 */
-	get from() {
-		return this._from;
-    }
-    
-    get content() {
+    this.title = name;
+		this.name = name;
+		this.from = from;
+    this.to = to;
+    this._start = Moment.unix(from/1000).format('YYYY-MM-DD HH:mm:ss');
+    this._end = Moment.unix(to/1000).format('YYYY-MM-DD HH:mm:ss');
+  }
+  
+  get content() {
 		return '<i class="v-icon material-icons">golf_course</i>';
 	}
 
-    get class() {
-		return 'bg-secondary text-light';
-    }
- 
-	/**
-	 * @type {string}
-	 */
-	get to() {
-		return this._to;
-    }
-    
-    get start() {
+  get class() {
+    return 'bg-secondary text-light';
+  }
+  
+  get start() {
 		return this._start;
 	}
 
