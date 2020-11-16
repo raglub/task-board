a1a7966e6b84eceff4199f23b85d5709c9cea95b
Michał Szyma <michal@szyma.dev>
2020-11-16 22:00:25 +0100

    Improve the logo

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,28 @@ module.exports = {
         electronBuilder: {
             builderOptions: {
                 appId: "dev.szyma.task-board",
+                productName: 'Task Board',
                 publish: {
                     provider: "github",
                     owner: "raglub",
                     repo: "task-board"
+                },
+                win: {
+                    icon: "assets/icons/win/icon.ico"
+                },
+                linux: {
+                    icon: 'assets/icons/linux/icon.icns',
+                    target: [
+                        {
+                          "target": "deb",
+                          "arch": [
+                            "x64",
+                            "ia32"
+                          ]
+                        }
+                    ],
+                    category: "Utility",
+                    packageCategory: "Utility"
                 }
             }
         }
