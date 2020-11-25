87906d5c371faf20ec14322fe1c5450885812e33
Michał Szyma <devszyma@gmail.com>
2020-11-26 00:57:50 +0100

    Add version to About view

--- src/utils/ipc-template.ts
+++ src/utils/ipc-template.ts
@@ -4,4 +4,5 @@ import Tag from '@/models/tag';
 export interface IpcTemplate {
     [IpcTypes.CreateTag] (name: string): Promise<Tag>;
     [IpcTypes.GetAllTags] (): Promise<Tag[]>;
+    [IpcTypes.GetVersion] (): Promise<string>;
 }
